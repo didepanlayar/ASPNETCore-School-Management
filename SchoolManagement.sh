2c490d93b2e8fc950bd6452b800edf1483b565fd
Di Depan Layar <albinmaulana@yahoo.com>
2023-08-16 09:33:43 +0700

    Feature: Add Package Microsoft.EntityFrameworkCore.Tools

--- SchoolManagement.sh
+++ SchoolManagement.sh
@@ -5,3 +5,4 @@ dotnet ef dbcontext scaffold 'Server=localhost,1400;Database=SchoolManagement;Tr
 dotnet ef dbcontext scaffold 'Server=localhost,1400;Database=SchoolManagement;Trusted_Connection=False;MultipleActiveResultSets=true;Encrypt=false;User Id=sa;Password=Rahasia@2023' Microsoft.EntityFrameworkCore.SqlServer -o Data -f --no-onconfiguring;
 dotnet tool install --global dotnet-aspnet-codegenerator;
 dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design;
+dotnet add package Microsoft.EntityFrameworkCore.Tools;
