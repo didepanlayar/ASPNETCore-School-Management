8c005d25b5544c591bb1575cedbabda82b6ae764
Di Depan Layar <albinmaulana@yahoo.com>
2023-08-16 09:18:34 +0700

    Feature: Add Package Microsoft.VisualStudio.Web.CodeGeneration.Design

--- SchoolManagement.sh
+++ SchoolManagement.sh
@@ -4,3 +4,4 @@ dotnet tool install --global dotnet-ef;
 dotnet ef dbcontext scaffold 'Server=localhost,1400;Database=SchoolManagement;Trusted_Connection=False;MultipleActiveResultSets=true;Encrypt=false;User Id=sa;Password=Rahasia@2023' Microsoft.EntityFrameworkCore.SqlServer -o Data;
 dotnet ef dbcontext scaffold 'Server=localhost,1400;Database=SchoolManagement;Trusted_Connection=False;MultipleActiveResultSets=true;Encrypt=false;User Id=sa;Password=Rahasia@2023' Microsoft.EntityFrameworkCore.SqlServer -o Data -f --no-onconfiguring;
 dotnet tool install --global dotnet-aspnet-codegenerator;
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design;
