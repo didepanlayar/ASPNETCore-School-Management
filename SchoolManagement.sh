db7ffa89da1b0b7a8488c1a9ecd3d68011f9475c
Di Depan Layar <albinmaulana@yahoo.com>
2023-08-25 10:37:50 +0700

    Feature: Generate Class View and Controller

--- SchoolManagement.sh
+++ SchoolManagement.sh
@@ -11,3 +11,4 @@ dotnet aspnet-codegenerator controller -name LecturersController -m Lecturer -dc
 dotnet aspnet-codegenerator controller -name StudentsController -m Student -dc SchoolManagementContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries -f;
 # SchoolManagement.sql Add Classes and Enrollments Tables to the Database
 dotnet ef dbcontext scaffold 'Server=localhost,1400;Database=SchoolManagement;Trusted_Connection=False;MultipleActiveResultSets=true;Encrypt=false;User Id=sa;Password=Rahasia@2023' Microsoft.EntityFrameworkCore.SqlServer -o Data -f --no-onconfiguring;
+dotnet aspnet-codegenerator controller -name ClassesController -m Class -dc SchoolManagementContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries -f;
